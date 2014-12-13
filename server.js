93affbb16e90e8702fba53de9d8b526efef543ee
qurihara <qurihara@gmail.com>
2014-12-13 22:45:56 +0900

    control.html added

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var settings = require('./settings');
 
 var app = require('http').createServer(handler),
-	io = require('socket.io').listen(app,{'log level': 1});
+	io = require('socket.io').listen(app,{'log level': 1}),
+  fs = require('fs');
 app.listen(settings.port);
 
 var http = require('http');
@@ -11,15 +12,29 @@ function handler(req,res){
 	if (req.url.lastIndexOf('/delay_on', 0) === 0){
       io.sockets.emit('delay_status',true);
       console.log("delay_status true emitted.");
+      res.writeHead(200);
+      res.write("ok");
+      res.end();
+      return;
   }
   if (req.url.lastIndexOf('/delay_off', 0) === 0){
       io.sockets.emit('delay_status',false);
       console.log("delay_status false emitted.");
+      res.writeHead(200);
+      res.write("ok");
+      res.end();
+      return;
   }
-  res.writeHead(200);
-  res.write("ok");
-  res.end();
-  return;
+
+  fs.readFile(__dirname + req.url,function(err,data){
+    if(err){
+     res.writeHead(500);
+     return res.end('Error: ' + err);
+    }
+    res.writeHead(200);
+    res.write(data);
+    res.end();
+  });
 }
 
 io.sockets.on('connection', function (socket) {

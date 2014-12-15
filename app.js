da2f8add81a33fe3a82fbdd93251035ad7cac4be
Kazutaka Kurihara <=>
2014-12-15 14:42:49 +0900

    worked

--- app.js
+++ app.js
@@ -12,19 +12,36 @@ socc.on('connect', function (data) {
 socc.on('delay_status', function (msg) {
   debug_log("recieved delay_status : " + msg);
   delay = msg;
+  update();
 });
 
-process.stdin.on('data',function(chunk){
-        if (!delay){
-                process.stdout.write(chunk);
+var spawn = require('child_process').spawn;
+var exec = require ('child_process').exec;
+var ps = null;
+function update(){
+    exec('pkill aplay', function(err,stdout, stderr){
+	console.log('kill aplay ' + err+stdout+stderr);
+//      exec('pkill arecord', function(err,stdout, stderr){
+//	console.log('kill arecord ' + err+stdout+stderr);
+        var cmd;
+        if(!delay) {
+          cmd = "arecord -D hw:1,0 -f S16_LE -r 16000 | aplay -R 1";
+          debug_log("delay off");
+        } else{
+          cmd = "arecord -D hw:1,0 -f S16_LE -r 16000 | aplay";
+          debug_log("delay on");
         }
-        else
-        {
-                setTimeout(function(){
-                        process.stdout.write(chunk);
-                }, settings.delay);
-        }
-});
+        exec(cmd,function(err,stdout, stderr){
+    	  console.log('command exec ' + err+stdout+stderr);
+        });
+        //ps = shspawn(cmd);
+//      });
+    });
+}
+
+function shspawn(command) {
+  return spawn('sh', ['-c', command]);
+}
 
 function debug_log(msg){
   if (settings.debug){
@@ -32,3 +49,4 @@ function debug_log(msg){
   }
 }
 
+update();

7eb6826fe46465889941a462b940363659555c97
Kazutaka Kurihara <=>
2014-12-15 15:21:33 +0900

    modified for usb headset

--- app.js
+++ app.js
@@ -25,10 +25,12 @@ function update(){
 //	console.log('kill arecord ' + err+stdout+stderr);
         var cmd;
         if(!delay) {
-          cmd = "arecord -D hw:1,0 -f S16_LE -r 16000 | aplay -R 1";
+//          cmd = "arecord -D hw:1,0 -f S16_LE -r 16000 | aplay -R 1"; // for built-in headphone
+          cmd = "arecord -D hw:1,0 -f S16_LE -r 16000 | aplay -R 1 -D plughw:1,0"; // for usb headphone
           debug_log("delay off");
         } else{
-          cmd = "arecord -D hw:1,0 -f S16_LE -r 16000 | aplay";
+//          cmd = "arecord -D hw:1,0 -f S16_LE -r 16000 | aplay"; // for built-in headphone
+          cmd = "arecord -D hw:1,0 -f S16_LE -r 16000 | aplay -D plughw:1,0"; // for usb headphone
           debug_log("delay on");
         }
         exec(cmd,function(err,stdout, stderr){
